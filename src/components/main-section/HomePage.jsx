7d92ce05a9c3b761bfd200496a3b56f0033283ff
Elochukwu Aroh <elochukwuaroh@gmail.com>
2023-01-02 09:23:44 -0600

    create TechStackContainer component and use

--- src/components/main-section/HomePage.jsx
+++ src/components/main-section/HomePage.jsx
@@ -1,31 +1,18 @@
 import React from 'react';
-import { Card} from 'antd';
 import { Link } from 'react-router-dom';
-
 import { Button, Layout} from 'antd';
 
+import TechStackContainer from './TechStackContainer';
 import styles from './homePage.module.css'
+
 const { Content } = Layout;
 
 const HomePage = () => {
   return ( 
     <Content >
       <div className={styles['main-section-container']}>
-        <div className={styles['main-section-inner-container']}>
-          <div className={styles['stack-outer-container']} >
-
-            <Card title="Tech Stack Arsenal" bordered={false}>
-              <div className={styles['stack-inner-container']}>              
-                <p className={styles['stack-option']}>React.Js</p>
-                <p className={styles['stack-option']}>ASP.Net Core</p>
-                <p className={styles['stack-option']}>Node.js</p>
-                <p className={styles['stack-option']}>MS SQL Server </p>
-                <p className={styles['stack-option']}>Git</p>
-              </div>
-            </Card>           
-
-          </div>
-          
+        <div className={styles['main-section-inner-container']}>  
+          <TechStackContainer/>            
           <h1 className={styles['main-text']}>Hey, i'm Elochukwu Aroh</h1>
           <p className={styles['supporting-text']}>You want laser focus attention to detail with your projects?</p>
           <p className={styles['supporting-text']}>Well look no further!</p>
---
+++ src/components/main-section/TechStackContainer.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { Card } from "antd";
+
+import styles from './techStackContainer.module.css';
+
+const TechStackContainer = () => {
+  return (
+    <div className={styles['stack-outer-container']} >
+
+      <Card title="Tech Stack Arsenal" bordered={false}>
+        <div className={styles['stack-inner-container']}>              
+          <p className={styles['stack-option']}>React.Js</p>
+          <p className={styles['stack-option']}>ASP.Net Core</p>
+          <p className={styles['stack-option']}>Node.js</p>
+          <p className={styles['stack-option']}>MS SQL Server </p>
+          <p className={styles['stack-option']}>Git</p>
+        </div>
+      </Card>           
+
+    </div>
+    );
+}
+ 
+export default TechStackContainer;
