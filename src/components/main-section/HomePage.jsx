fba94739b8449df8bcff7e8a2785c04a14e58ceb
Elochukwu Aroh <elochukwuaroh@gmail.com>
2023-01-02 09:08:27 -0600

    add tech stack display container

--- src/components/main-section/HomePage.jsx
+++ src/components/main-section/HomePage.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Card} from 'antd';
 import { Link } from 'react-router-dom';
 
 import { Button, Layout} from 'antd';
@@ -11,6 +12,20 @@ const HomePage = () => {
     <Content >
       <div className={styles['main-section-container']}>
         <div className={styles['main-section-inner-container']}>
+          <div className={styles['stack-outer-container']} >
+
+            <Card title="Tech Stack Arsenal" bordered={false}>
+              <div className={styles['stack-inner-container']}>              
+                <p className={styles['stack-option']}>React.Js</p>
+                <p className={styles['stack-option']}>ASP.Net Core</p>
+                <p className={styles['stack-option']}>Node.js</p>
+                <p className={styles['stack-option']}>MS SQL Server </p>
+                <p className={styles['stack-option']}>Git</p>
+              </div>
+            </Card>           
+
+          </div>
+          
           <h1 className={styles['main-text']}>Hey, i'm Elochukwu Aroh</h1>
           <p className={styles['supporting-text']}>You want laser focus attention to detail with your projects?</p>
           <p className={styles['supporting-text']}>Well look no further!</p>
