8d9758b2982904cc5bda268d4609ceae086fa34a
Elochukwu Aroh <elochukwuaroh@gmail.com>
2023-02-10 19:56:46 -0600

    complete about page

--- src/components/main-section/AboutPage.jsx
+++ src/components/main-section/AboutPage.jsx
@@ -1,18 +1,36 @@
 import React from 'react';
-import { Avatar } from 'antd';
+import { EditOutlined, EllipsisOutlined, SettingOutlined } from '@ant-design/icons';
+import { AutoComplete, Avatar, Card } from 'antd';
+
 
 import eloPicture from '../../assets/user-images/professional-pic.jpeg';
 
+const { Meta } = Card;
 
 const AboutPage = () => {
   return ( 
     <React.Fragment>
-      <h3>Page build in progress, would be available shortly</h3>
-      <Avatar
-        size={{xxl: 200}}
-        src= {eloPicture}
-        
-       />
+      <Card
+        style={{width: 300 ,left:'40vw', right:'40vw', top: '2em' }}
+        cover={
+          <img
+            alt="A picture of Elochukwu Aroh"
+            src={eloPicture}
+          />
+         }
+        actions={[
+          <SettingOutlined key="setting" />,
+          <EditOutlined key="edit" />,
+          <EllipsisOutlined key="ellipsis" />,
+        ]}
+      >
+        <Meta
+          
+          title="About"
+          description="Hello there! My Name is Elochukwu Aroh. I am Software Developer passionate about stamping my feet in the sands of time in my own little way, for fun I like board games, table tennis and soccer. Thanks for stopping by!"
+        />
+      </Card>
+      
       
 
     </React.Fragment>
