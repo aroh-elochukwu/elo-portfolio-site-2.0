332e4568327bb7b1eead4afb2d2665ea84b1a037
Elochukwu Aroh <elochukwuaroh@gmail.com>
2023-02-10 19:30:55 -0600

    add ugly picture to about page

--- src/components/main-section/AboutPage.jsx
+++ src/components/main-section/AboutPage.jsx
@@ -1,10 +1,18 @@
 import React from 'react';
+import { Avatar } from 'antd';
+
+import eloPicture from '../../assets/user-images/professional-pic.jpeg';
 
 
 const AboutPage = () => {
   return ( 
     <React.Fragment>
       <h3>Page build in progress, would be available shortly</h3>
+      <Avatar
+        size={{xxl: 200}}
+        src= {eloPicture}
+        
+       />
       
 
     </React.Fragment>
