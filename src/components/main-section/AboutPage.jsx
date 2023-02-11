2b17c78a7405c8f9b59fd6dddff1aea4e6a6f6f2
Elochukwu Aroh <elochukwuaroh@gmail.com>
2023-02-10 19:04:40 -0600

    move hidden page content from about page

--- src/components/main-section/AboutPage.jsx
+++ src/components/main-section/AboutPage.jsx
@@ -1,17 +1,11 @@
 import React from 'react';
-import { DownloadOutlined } from '@ant-design/icons';
-import { Button } from 'antd';
 
-import resume from '../../assets/files/Aroh-Elochukwu-resume.pdf'
 
 const AboutPage = () => {
   return ( 
     <React.Fragment>
       <h3>Page build in progress, would be available shortly</h3>
-      <p>Please note that the download button below is a personal file sharing resource and you should only click if instructed so, thank you.</p>
-      <Button href={resume} style={{width:'10em'}} type="primary" icon={<DownloadOutlined />} size='large'>
-        Download
-      </Button>
+      
 
     </React.Fragment>
     
--- src/components/main-section/HiddenPage.jsx
+++ src/components/main-section/HiddenPage.jsx
@@ -1,7 +1,19 @@
 import React from 'react'
+import { DownloadOutlined } from '@ant-design/icons';
+import { Button } from 'antd';
+
+import resume from '../../assets/files/Aroh-Elochukwu-resume.pdf'
+
+
 
 export default function HiddenPage() {
   return (
-    <div>HiddenPage</div>
+    
+    <React.Fragment>
+      <p>Please note that the download button below is a personal file sharing resource and you should only click if instructed so, thank you.</p>
+      <Button href={resume} style={{width:'10em'}} type="primary" icon={<DownloadOutlined />} size='large'>
+        Download
+      </Button>
+    </React.Fragment>
   )
 }
